6fe17862e29deaf2c6da317f1232c7de9bffe31e
Whp Krts <shpynx@hotmail.com>
2017-10-08 22:00:05 +0300

    tr.php
    Turkish translation of category page.

---
+++ po-content/lang/category/tr.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * - PopojiCMS Category Language
+ *
+ * - File : tr.php
+ * - Version : 1.0
+ * - Author : Vahap Karataş
+ * - License : MIT License
+ *
+*/
+
+$ _ ['component_name'] = 'Kategori';
+$ _ ['category_parent'] = 'Ebeveyn';
+$ _ ['category_title'] = 'Başlık | Bağlantı ';
+$ _ ['category_title_2'] = 'Başlık';
+$ _ ['category_picture'] = 'Resim';
+$ _ ['category_active'] = 'Etkin';
+$ _ ['category_action'] = 'Eylem';
+$ _ ['category_addnew'] = 'Kategori Ekle';
+$ _ ['category_edit'] = 'Kategoriyi Güncelle';
+$ _ ['category_message_1'] = 'Kategori başarıyla eklendi';
+$ _ ['category_message_2'] = 'Kategori başarıyla güncellendi';
+$ _ ['category_message_3'] = 'Kategori başarıyla silindi';
+$ _ ['category_message_4'] = 'Yeni kategori verileri eklenirken hata oluştu';
+$ _ ['category_message_5'] = 'Kategori verisi güncellenirken hata oluştu';
+$ _ ['category_message_6'] = 'Kategori veri silinirken hata oluştu';
+$ _ ['front_category_title'] = 'Kategori';
+$ _ ['front_all_category'] = 'Tüm Kategori';
+$ _ ['front_category_not_found'] = 'Kategori Bulunamadı';
